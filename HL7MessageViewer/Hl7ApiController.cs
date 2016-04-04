8c05cffd6f7df5fc84ebf6385876f2df38c95a63
Acharya Raja Sekhar Brundhavanam <acharya.r@live.com>
2016-04-05 00:31:20 +0530

    From VS

---
+++ HL7MessageViewer/Hl7ApiController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HL7MessageViewer
+{
+    public class Hl7ApiController : ApiController
+    {
+        public long Get(long messageid)
+        {
+            return messageid;
+        }
+
+        public MyHL7Segment[] Get(string url)
+        {
+            MyHL7Segment[] segments = new MyHL7Segment[]
+            {
+                new MyHL7Segment{
+                    Title = "MHS",
+                    Fields = new Field[] 
+                    {
+                        new Field
+                        {
+                            Id = "1",
+                            Key = "Key",
+                            Value = "Value"
+                        },
+                        new Field
+                        {
+                            Id = "1",
+                            Key = "Key",
+                            Value = "Value"
+                        },
+                        new Field
+                        {
+                            Id = "1",
+                            Key = "Key",
+                            Value = "Value"
+                        }
+                    }
+                },
+                new MyHL7Segment{
+                    Title = "EVN",
+                    Fields = new Field[] 
+                    {
+                        new Field
+                        {
+                            Id = "1",
+                            Key = "Key",
+                            Value = "Value"
+                        },
+                        new Field
+                        {
+                            Id = "1",
+                            Key = "Key",
+                            Value = "Value"
+                        },
+                        new Field
+                        {
+                            Id = "1",
+                            Key = "Key",
+                            Value = "Value"
+                        }
+                    }
+                },
+                new MyHL7Segment{
+                    Title = "PV1",
+                    Fields = new Field[] 
+                    {
+                        new Field
+                        {
+                            Id = "1",
+                            Key = "Key",
+                            Value = "Value"
+                        },
+                        new Field
+                        {
+                            Id = "1",
+                            Key = "Key",
+                            Value = "Value"
+                        },
+                        new Field
+                        {
+                            Id = "1",
+                            Key = "Key",
+                            Value = "Value"
+                        }
+                    }
+                }
+            };
+
+            return segments;
+        }
+
+        public string[] GetMessagesByPasID(string server, string ns, string pasid)
+        {
+            Random r = new Random();
+            var id = new string[100];
+            for (int i = 0; i < 100; i++)
+            {
+                id[i] = r.Next().ToString();
+            }
+
+            return id;
+        }
+    }
+
+    public class MyHL7Segment
+    {
+        public string Title { get; set; }
+
+        public Field[] Fields { get; set; }
+    }
+
+    public class Field
+    {
+        public string Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ HL7MessageViewer/Scripts/Js/Script.js
@@ -0,0 +1,70 @@
+﻿/// <reference path="../angular.min.js" />
+var app = angular.module("hl7MsgVwr", []);
+
+app.factory("hl7MsgSvc", function ($http) {
+    var result;
+
+
+});
+
+app.controller("hl7MsgCtrl", function ($scope, $rootScope, $http, $log, $anchorScroll, $location) {
+
+    $scope.OnFormSubmit = function () {
+
+        $rootScope.Server = $scope.Server;
+        $rootScope.NameSpace = $scope.NameSpace;
+        $rootScope.PasID = $scope.PasID;
+
+        $scope.Loading++;
+
+        $http({
+            url: "api/Hl7Api",
+            method: "GET",
+            params: {
+                server: $scope.Server,
+                ns: $scope.NameSpace,
+                pasid: $scope.PasID
+            }
+        })
+            .success(function (response, status, headers, config) {
+                $scope.MsgIDs = response;
+                $scope.Error = "";
+                $log.info(response);
+                $scope.Loading--;
+            })
+            .error(function (response, status, headers, config) {
+                $scope.Error = response.ExceptionMessage;
+                $log.error(response);
+                $scope.Loading--;
+            });
+
+    };
+
+    $scope.OnMsgIDClick = function (msgID) {
+        $scope.CurrentMessageID = msgID;
+        $scope.Loading++;
+        $http({
+            url: "api/Hl7Api",
+            method: "GET",
+            params: {
+                url: ""
+            }
+        }).success(function (response, status, headers, config) {
+            $scope.HL7MsgSegments = response;
+            $scope.Error = "";
+            $log.info(response);
+            $scope.Loading--;
+        }).error(function (response, status, headers, config) {
+            $scope.Error = response.ExceptionMessage;
+            $log.error(response);
+            $scope.Loading--;
+        });
+    };
+
+    $scope.Server = "";
+    $scope.NameSpace = "";
+    $scope.PasID = "";
+    $scope.Error = "";
+    $scope.CurrentMessageID = "";
+    $scope.Loading = 0;
+});
